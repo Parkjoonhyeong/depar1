fc68060e18f0fef5770598821e363cd616c103a6
Joonhyeong Park <pjh010919@gmail.com>
2023-11-07 10:51:08 +0900

    final

--- src/components/CartSidebar.js
+++ src/components/CartSidebar.js
@@ -1,7 +1,7 @@
 import { addToCart, removeFromCart } from '@/app/redux/slices/cartSlice'
 import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
+import { usePathname } from 'next/navigation'
 import { useDispatch, useSelector } from 'react-redux'
 
 export default function CartSidebar() {
@@ -9,66 +9,82 @@ export default function CartSidebar() {
 
   const dispatch = useDispatch()
 
-  const addToCartHandler = async (product, qty) => {
+  const addToCartHandler = (product, qty) => {
     dispatch(addToCart({ ...product, qty }))
   }
 
-  const removeFromeCartHandler = (id) => {
+  const removeFromCartHandler = (id) => {
     dispatch(removeFromCart(id))
   }
 
+  const pathname = usePathname()
+
   return (
-    <div className="fixed top-0 right-0 w-32 h-full shadow-lg border-l border-l-gary-700 overflow-scroll">
+    <div
+      className={
+        loading
+          ? ''
+          : cartItems.length > 0 &&
+            (pathname === '/' || pathname.indexOf('/product/') >= 0)
+          ? 'fixed top-0 right-0 w-32 h-full shadow-lg border-l border-l-gary-700 overflow-scroll'
+          : 'hidden'
+      }
+    >
       {loading ? (
-        <div className="py-4 px-2">Loading...</div>
+        <div className="py-5 px-2">Loading...</div>
       ) : cartItems.length === 0 ? (
         <div className="py-5 px-2">Cart is empty</div>
       ) : (
         <>
-          <div classNamel="p=2 flex-col items-center border-b border-b-gray-600">
-            <div>Subtotal</div>
-            <div className="font-bold text-orange-700">{itemsPrice}</div>
-            <Link
-              href="/cart"
-              className="w-full text-center p-1 rounded-2xl border-2"
-            >
-              Go to cart
-            </Link>
-          </div>
-          {cartItems.map((item) => (
-            <div
-              key={item.id}
-              className="p-2 flex flex-col items-center border-b border-b-gray-600"
-            >
-              <Link href={`/product/${item.id}`} className="flex items=center">
-                <Image
-                  src={item.image}
-                  alt={item.name}
-                  width={50}
-                  height={50}
-                  className="p-1"
-                />
+          <div className="p-2 flex flex-col items-center border-b border-b-gary-600">
+            <div>subtotal</div>
+            <div className="font-bold text-orange-700">${itemsPrice}</div>
+            <div>
+              <Link
+                href="/cart"
+                className="w-full text-center p-1  rounded-2xl border-2"
+              >
+                Go to cart
               </Link>
-
-              <select
-                value={item.qty}
-                onChange={(e) => addToCartHandler(item, Number(e.target.value))}
-              >
-                {[...Array(item.countInStock).keys()].map((x) => (
-                  <option key={x + 1} value={x + 1}>
-                    {x + 1}
-                  </option>
-                ))}
-              </select>
-
-              <button
-                className="default-button mt-2"
-                onClick={() => removeFromeCartHandler(item.id)}
-              >
-                Delete
-              </button>
             </div>
-          ))}
+            {cartItems.map((item) => (
+              <div
+                key={item.id}
+                className="p-2 flex flex-col items-center border-b border-b-gary-600"
+              >
+                <Link
+                  href={`/product/${item.id}`}
+                  className="flex items-center"
+                >
+                  <Image
+                    src={item.image}
+                    alt={item.name}
+                    width={50}
+                    height={50}
+                    className="p-1"
+                  ></Image>
+                </Link>
+                <select
+                  value={item.qty}
+                  onChange={(e) =>
+                    addToCartHandler(item, Number(e.target.value))
+                  }
+                >
+                  {[...Array(item.countInStock).keys()].map((x) => (
+                    <option key={x + 1} value={x + 1}>
+                      {x + 1}
+                    </option>
+                  ))}
+                </select>
+                <button
+                  className="default-button mt-2"
+                  onClick={() => removeFromCartHandler(item.id)}
+                >
+                  Delete
+                </button>
+              </div>
+            ))}
+          </div>
         </>
       )}
     </div>
